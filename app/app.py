d28a1d69d1000661f7ac8b0ebf02370201e71ce6
sda1003 <sda1003@alu.ubu.es>
2023-11-21 20:53:52 +0100

    Formulario

--- app/app.py
+++ app/app.py
@@ -1,16 +1,26 @@
 from flask import Flask, render_template, request
+import form
+
 
 #Inicializar aplicación
 app = Flask(__name__)
 
-@app.route('/')
+
+@app.route('/', methods = ['GET', 'POST'])
 def index():
     #return "Hola Mundo!" #Antes de crear index.html
 
     nombre = "Sandra" #Pasar variables al html
     num = 1
     lista=[1,2,3,4,5,6,7]
-    return render_template('index.html', nombre=nombre, num=num, lista=lista)
+    comment_form = form.CommentForm(request.form)
+    if request.method == 'POST' and comment_form.validate():
+        print(comment_form.username.data)
+        print(comment_form.idPaciente.data)
+    else:
+        print("Error en el formulario.")
+
+    return render_template('index.html', nombre=nombre, num=num, lista=lista, form=comment_form)
 
 
 
@@ -20,7 +30,6 @@ def acceso():
 
     idPaciente = request.form.get("idPaciente")
     return render_template('acceso.html', idPaciente=idPaciente)
-
 
 
 if __name__=='__main__':
---
+++ app/form.py
@@ -0,0 +1,22 @@
+from wtforms import Form, validators
+from wtforms import StringField, DecimalField, HiddenField
+
+
+def length_honeypot(form, field):
+    if len(field.data) > 0:
+        raise validators.ValidationError('El campo debe estar vacío.')
+
+class CommentForm(Form):
+    #Acepta strings
+    username = StringField('username',
+                            [validators.DataRequired(message='Campo obligatorio'),
+                             validators.length(min=4, message='Ingrese un username válido')
+                         ])
+    #Acepta numeros
+    idPaciente = DecimalField('Identificador de paciente',
+                              [validators.DataRequired(message='Campo obligatorio'),
+                               validators.NumberRange(min=0, max=100, message='Ingrese un id válido')
+                            ])
+    
+    #Campo oculto para los usurios que ayuda contra ataques
+    honeypot = HiddenField('', [length_honeypot])
