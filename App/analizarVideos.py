32e3b32303f8d83705a7c9503a012188aec8466b
sda1003 <sda1003@alu.ubu.es>
2024-05-06 17:11:18 +0200

    Análisis de prueba

--- App/analizarVideos.py
+++ App/analizarVideos.py
@@ -5,7 +5,7 @@ from modelosbbdd import Videos
 
 #Importar funcion de Catalin
 from paddel.src.paddel.preprocessing.input.poses import extract_poses_ts
-
+from paddel.src.paddel.preprocessing.input.classic import extract_classic_features
 
 
 #Conexión a la base de datos
@@ -14,29 +14,45 @@ Session = sessionmaker(bind=engine)
 session = Session()
 
 def analizarVideos():
+
+    video_path = "static/videos/1/CONTROL995_03-08-2023_DCHA_M-56-D.mp4"
+    poses_df = extract_poses_ts(video_path)
+    #print(poses_df)
+    caracteristicas = extract_classic_features(poses_df)
+    print(caracteristicas)
+
+
+
+
     #Todos los videos de la base de datos que no tienen todavía características
-    videos = session.query(Videos).filter(Videos.estadisticas.is_(None)).all()
+    # videos = session.query(Videos).filter(Videos.velocidad_media.is_(None)).all()
 
-    for video in videos:
-        video_path = video.contenido  #Ruta del video para pasar a la función
-        video_id = video.id_video  #ID del video para control
+    # for video in videos:
+    #     video_id = video.id_video  #ID del video para control
+    #     video_path = "static/videos/" + str(video.paciente) + "/" + video.contenido #Ruta al video
 
-        try:
-            #Extrae las poses de la mano del vídeo con tiempos
-            poses_df = extract_poses_ts(video_path)
+    #     try:
+    #         #Extrae las poses de la mano del vídeo con tiempos
+    #         poses_df = extract_poses_ts(video_path)
 
-            #Convertir el DataFrame a JSON para guardarlo
-            poses_json = poses_df.to_json()
+    #         caracteristicas = extract_classic_features(poses_df)
 
-            #Actualizar bbdd
-            video.estadisticas = poses_json
-            session.commit()
+    #         #Actualizar bbdd con las 7 características devueltas por la función
+            # video.velocidad_media = caracteristicas["mean_speed"]
+            # video.frecuencia_max = caracteristicas["frequency_of_maximums"]
+            # video.frecuencia_min = caracteristicas["frequency_of_minimums"]
+            # video.promedio_max = caracteristicas["average_of_maximums"]
+            # video.desv_estandar_max = caracteristicas["std_of_maximums"]
+            # video.diferencia_ranurada_min = caracteristicas["slotted_difference_of_frequency_of_minimums"]
+            # video.diferencia_ranurada_max = caracteristicas["slotted_difference_of_average_of_maximums"]
 
-            # Imprimir algunas estadísticas para el video actual
-            print(f"Video ID: {video_id}, Numero de frames: {len(poses_df)}")
+    #         session.commit()
 
-        except Exception as e:
-            print(f"Error procesando el vídeo con ID {video_id}: {str(e)}")
+    #         # Imprimir algunas estadísticas para el video actual
+    #         print(f"Video ID: {video_id}, Numero de frames: {len(poses_df)}")
+
+    #     except Exception as e:
+    #         print(f"Error procesando el vídeo con ID {video_id}: {str(e)}")
 
 if __name__ == "__main__":
     analizarVideos()
